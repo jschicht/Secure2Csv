81d88eafbd07873c14cfa05509a092924039cdd7
Joakim Schicht <joakim.schicht@gmail.com>
2016-06-17 00:32:07 +0200

    Improved db schema for secure table.

--- Secure-schema.sql
+++ Secure-schema.sql
@@ -1,27 +1,29 @@
 
 CREATE TABLE secure(
-   Offset						VARCHAR(18)
-  ,SecurityDescriptorHash		VARCHAR(10)
-  ,SecurityId					INTEGER 
-  ,Control						MEDIUMTEXT 
-  ,SidOwner						VARCHAR(128)
-  ,SidGroup						VARCHAR(128)
-  ,SAclRevision					INTEGER
-  ,SAceCount					INTEGER
-  ,SAceType						MEDIUMTEXT
-  ,SAceFlags					MEDIUMTEXT
-  ,SAceMask						MEDIUMTEXT
-  ,SAceObjectFlags				MEDIUMTEXT
-  ,SAceObjectType				MEDIUMTEXT
-  ,SAceInheritedObjectType		MEDIUMTEXT 
-  ,SAceSIDofTrustee				MEDIUMTEXT 
-  ,DAclRevision					INTEGER 
-  ,DAceCount					INTEGER
-  ,DAceType						MEDIUMTEXT 
-  ,DAceFlags					MEDIUMTEXT 
-  ,DAceMask						MEDIUMTEXT 
-  ,DAceObjectFlags				MEDIUMTEXT 
-  ,DAceObjectType				MEDIUMTEXT 
-  ,DAceInheritedObjectType		MEDIUMTEXT
-  ,DAceSIDofTrustee				MEDIUMTEXT 
+	`Id`						INT(11) NOT NULL AUTO_INCREMENT
+	,`Offset`					VARCHAR(18)
+	,`SecurityDescriptorHash`	VARCHAR(10)
+	,`SecurityId`				INTEGER 
+	,`Control`					MEDIUMTEXT 
+	,`SidOwner`					VARCHAR(128)
+	,`SidGroup`					VARCHAR(128)
+	,`SAclRevision`				INTEGER
+	,`SAceCount`				INTEGER
+	,`SAceType`					MEDIUMTEXT
+	,`SAceFlags`				MEDIUMTEXT
+	,`SAceMask`					MEDIUMTEXT
+	,`SAceObjectFlags`			MEDIUMTEXT
+	,`SAceObjectType`			MEDIUMTEXT
+	,`SAceInheritedObjectType`	MEDIUMTEXT 
+	,`SAceSIDofTrustee`			MEDIUMTEXT 
+	,`DAclRevision`				INTEGER 
+	,`DAceCount`				INTEGER
+	,`DAceType`					MEDIUMTEXT 
+	,`DAceFlags`				MEDIUMTEXT 
+	,`DAceMask`					MEDIUMTEXT 
+	,`DAceObjectFlags`			MEDIUMTEXT 
+	,`DAceObjectType`			MEDIUMTEXT 
+	,`DAceInheritedObjectType`	MEDIUMTEXT
+	,`DAceSIDofTrustee`			MEDIUMTEXT
+	,PRIMARY KEY (Id)
 );
